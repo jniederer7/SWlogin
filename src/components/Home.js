7539d1bb8d9fe85eec169831cbcd158c3f016545
Julian Hasse <info@julianhasse.com>
2018-02-10 16:14:17 -0500

    Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ export default class Home extends Component {
   render () {
     return (
       <div>
-        Home. Not Protected. Anyone can see this.
+        Please register or login to be able to acess.
       </div>
     )
   }
