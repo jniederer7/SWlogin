098b2d89af15e2f93894289e83ac33466355d059
Julian Hasse <info@julianhasse.com>
2018-02-10 13:29:19 -0500

    initial repo

--- src/components/protected/Dashboard.js
+++ src/components/protected/Dashboard.js
@@ -5,7 +5,7 @@ export default class Dashboard extends Component {
     return (
       <div>
         <p>Welcome to StockWatch. You are securely logged in!</p>
-        <a href="http://localhost:3000"><img src="images/back.jpg" /></a>
+        <a href="https://stockwatch-main.herokuapp.com/"><img src="images/back.jpg" /></a>
       </div>
     )
   }
